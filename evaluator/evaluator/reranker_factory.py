4035fbaec843d12d2a6c54fa6ff5b873b6415fe3
Yuichi Tateno <hotchpotch@gmail.com>
2024-09-07 10:29:51 +0900

    Add for ruri-small, base, large

--- evaluator/evaluator/reranker_factory.py
+++ evaluator/evaluator/reranker_factory.py
@@ -121,6 +121,14 @@ def reranker_factory(model_name: str, device: str = "auto", use_fp16=True, kwarg
             model_name,
             device=device,
             use_fp16=use_fp16,
+    elif "cl-nagoya/ruri-" in reranker_name:
+        # for ruri-small, ruri-base, ruri-large models
+        return SentenceTransformerReranker(
+            model_name,
+            device=device,
+            use_fp16=use_fp16,
+            query_prefix="クエリ: ",
+            document_prefix="文章: ",
         )
     else:
         return SentenceTransformerReranker(model_name, device=device, use_fp16=use_fp16)
