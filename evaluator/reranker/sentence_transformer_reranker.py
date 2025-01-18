da17db7f3e029207559d8036970b97b58f1a85d9
Yuichi Tateno <hotchpotch@gmail.com>
2025-01-18 13:52:14 +0900

    Fix for sentence-embeddings

--- evaluator/reranker/sentence_transformer_reranker.py
+++ evaluator/reranker/sentence_transformer_reranker.py
@@ -16,14 +16,20 @@ class SentenceTransformerReranker(BaseReranker):
         max_seq_length=512,
     ):
         device = self._detect_device(device)
-        self.model = SentenceTransformer(model_name, device=device, trust_remote_code=True)
+        self.model = SentenceTransformer(
+            model_name, device=device, trust_remote_code=True
+        )
         if use_fp16 and "cuda" in device:
             self.model.half()
 
         self.query_prefix = query_prefix
         self.document_prefix = document_prefix
         self.normalize_embeddings = normalize_embeddings
-        self.model.max_seq_length = max_seq_length
+        try:
+            self.model.max_seq_length = max_seq_length
+        except AttributeError as e:
+            print("max_seq_length is not supported for this model")
+            pass
 
     def _rerank(self, query: str, documents: list[str]) -> list[float]:
         query_prefix = self.query_prefix
@@ -36,6 +42,7 @@ class SentenceTransformerReranker(BaseReranker):
         query_emb = embs[0]
         document_embs = embs[1:]
         scores = torch.nn.functional.cosine_similarity(
-            query_emb, document_embs  # type: ignore
+            query_emb,
+            document_embs,  # type: ignore
         ).tolist()
         return scores
